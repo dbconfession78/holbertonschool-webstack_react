9b239f57f03e13fc3d8b48472952b66492e4de89
Stuart Kuredjian <sgkur04@gmail.com>
2018-10-14 09:24:55 -0700

    first commit with functioning game

--- 0x01-state/101-tictactoe/src/components/Board.jsx
+++ 0x01-state/101-tictactoe/src/components/Board.jsx
@@ -1,11 +1,133 @@
 import React from 'react'
+import Square from './Square.jsx'
 
 export default class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: [["", "", ""], ["", "", ""], ["", "", ""]],
+            player: "X",
+            shouldReset: false,
+        }
+        this.squareClicked = this.squareClicked.bind(this)
+        this.TEST_CLICK = this.TEST_CLICK.bind(this)
+        this.onResetClick = this.onResetClick.bind(this)
+        // this.shouldReset = false;
+
+    }
+
+    setPlayer(player) {
+        this.setState({player: this.state.player === 'X' ? 'O' : 'X'})
+    }
+    squareClicked(player, id) {
+        const col = parseInt(id[0])
+        const row = parseInt(id[1])
+        let newSquares = this.state.squares
+        newSquares[row][col] = player
+        this.setState({
+            squares: newSquares
+        })
+        this.setPlayer()
+    }
+
+    checkBoard() {
+        let winner = this.checkHorizontal();
+        if (winner) {
+            alert("Winner: " + winner);
+        }
+        winner = this.checkVertical();
+        if (winner) {
+            alert("Winner: " + winner);
+        }
+        winner = this.checkDiagonal();
+        if (winner) {
+            alert("Winner: " + winner);
+        }
+        return 0;
+    }
+
+    checkHorizontal() {
+        const rows = this.state.squares
+        for(let i=0; i < rows.length; i++) {
+            const row = rows[i];
+            const first = row[0];
+            if (first !== "" && row[1] === first && row[2] === first){
+                return first;
+            }
+        }
+        return false;
+    }
+
+    checkVertical() {
+        const rows = this.state.squares;
+        for(let col=0; col < rows[0].length; col++) {
+            const first = rows[0][col];
+            const second = rows[1][col];
+            const third = rows[2][col]
+            if (first !== "" && second === first && third === first) {
+                return first;
+            }
+        }
+        return false;
+    }
+
+    checkDiagonal() {
+        const first = this.state.squares[0][0]
+        if (first !== "" && this.state.squares[1][1] === first && this.state.squares[2][2] === first) {
+            return first;
+        }
+        const third = this.state.squares[0][2]
+        if (third !== "" && this.state.squares[1][1] == third && this.state.squares[2][0] == third) {
+            return third;
+        }
+
+        return false;
+    }
+
+    resetBoard() {
+        this.setState({shouldReset: true})
+    }
+
+    onResetClick() {
+        this.resetBoard();
+    }
+
+    TEST_CLICK() {
+
+    }
+
+    componentDidUpdate() {
+        const winner = this.checkBoard();
+        if (winner != 0) {
+            alert("Winner: " + winner + "!");
+        }
+    }
+
     render() {
-        return (
-            <div className="Board">
-                
+        let c = 1;
+        let boardStyle = {
+            display: 'inline-flex',
+        }
+        let rows = this.state.squares.map((row, i) =>
+            <div key={i} className='row'>
+                {row.map((square, j) =>
+                    <Square
+                        state={this.state}
+                        value={this.state.squares[i][j]}
+                        player={this.state.player}
+                        onClick={this.squareClicked}
+                        id={i.toString() + j.toString()}
+                        key={j}/>
+                    )
+                }
             </div>
+        );
+        return (
+            <div style={boardStyle} className="Board">
+                {rows}
+                <button onClick={this.onResetClick}>Reset</button>
+            </div>
+
         )
     }
 }
--- 0x01-state/101-tictactoe/src/components/Square.jsx
+++ 0x01-state/101-tictactoe/src/components/Square.jsx
@@ -1,11 +1,65 @@
 import React from 'react';
 
 export default class Square extends React.Component {
-    render() {
-        return (
-            <div className="Square">
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: this.props.value,
+            player: "X",
+        }
+        this.didReset = false
+        this.onClick = this.onClick.bind(this);
+        this.onReset = this.onReset.bind(this);
+        this.player = this.props.player;
 
+    }
+
+    onClick(event) {
+        this.props.onClick(this.props.player, this.props.id)
+        this.setState({value: this.props.player})
+    }
+
+    onReset() {
+        alert()
+        this.setState({value: "", player: "X"});
+    }
+
+    render() {
+        const squareSize = '40px';
+        let squareStyle = {
+            border: 'black solid 1px',
+            width: squareSize,
+            height: squareSize,
+            margin: 'auto',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+            lineHeight: '40px'
+        }
+        return (
+            <div onClick={this.onClick} style={squareStyle} className="Square">
+                {this.state.value}
             </div>
         );
+    }
+
+    componentDidUpdate() {
+        if (this.didReset) {
+            this.didReset = false
+
+            // state shouldn't be set like this.
+            this.props.state.squares= [["", "", ""], ["", "", ""], ["", "", ""]];
+            this.props.state.shouldReset = false
+        }
+        if (this.props.state.shouldReset) {
+            this.didReset = true;
+        }
+        if (this.didReset) {
+            this.setState({value: ""})
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.props.parProps)
+        this.player = "X"
     }
 }
