74ca796a335782bb3f5e08f3ee2f8a9e444e294c
stuartkuredjian <Hyrenkosa1>
2018-10-14 16:02:57 -0700

    remove wrapper div around <Board />

--- 0x01-state/101-tictactoe/src/components/App.jsx
+++ 0x01-state/101-tictactoe/src/components/App.jsx
@@ -4,9 +4,7 @@ import Board from './Board.jsx'
 export default class App extends React.Component {
     render() {
         return (
-            <div id="root">
-                <Board />
-            </div>
+            <Board />
         );
     }
 }
